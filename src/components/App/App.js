eaf6f3dda9520f35e9b219fb88e09667944c3e69
Mariya Goncharenko <114483796+mariya-goncharenko@users.noreply.github.com>
2023-06-16 15:53:09 +0300

    Верстка регистрации и авторизации.
    Нужно поправить меню.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,19 +2,39 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import Header from "../Header/Header";
-import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
+
+import Main from "../Main/Main";
+import Register from "../Register/Register";
+import Login from "../Login/Login";
 
 function App() {
   return (
     <div className="page">
       <section className="page__content">
         <Switch>
+          {/* Главная страница */}
           <Route path="/" exact>
             <Header />
             <Main />
             <Footer />
           </Route>
+          {/* Регистрация */}
+          <Route path="/signup">
+            <Header />
+            <Register />
+            <Footer />
+          </Route>
+          {/* Авторизация */}
+          <Route path="/signin">
+            <Header />
+            <Login />
+            <Footer />
+          </Route>
+          
+          {/* Главная страница */}
+          {/* Главная страница */}
+          {/* Главная страница */}
         </Switch>
       </section>
     </div>
---
+++ src/components/Login/Login.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "../Form/Form.css";
+import Form from "../Form/Form";
+
+function Login() {
+  return (
+    <Form
+      title="Рады видеть!"
+      buttonText="Войти"
+      question="Еще не зарегистрированы?"
+      linkText=" Регистрация"
+      link="/signup"
+    >
+      <label className="form__field">
+        E-mail
+        <input
+          name="email"
+          className="form__input"
+          id="email-input"
+          type="email"
+          required
+        />
+        <span className="form__input-error">{}</span>
+      </label>
+      <label className="form__field">
+        Пароль
+        <input
+          name="password"
+          className="form__input"
+          id="password-input"
+          type="password"
+          required
+        />
+        <span className="form__input-error">{}</span>
+      </label>
+    </Form>
+  );
+}
+
+export default Login;
